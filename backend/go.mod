7a3110776b64c68d700814b0bf26c8381b30b090
Kenore1ka <nikitosproskyha@gmail.com>
2025-11-28 03:50:04 +0700

    feat: Create initial Go backend with health check endpoint

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/Kenore1ka/C2_Course_DnD-Character-Sheet/backend
+
+go 1.22.2
---
+++ backend/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// healthCheckHandler - это наш обработчик запросов.
+// Он будет вызываться каждый раз, когда кто-то заходит на нужный нам URL.
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Устанавливаем заголовок, чтобы клиент знал, что мы отвечаем в формате JSON.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Создаем структуру для ответа.
+	// `json:"status"` - это тег, который говорит, как назвать это поле в JSON.
+	response := struct {
+		Status string `json:"status"`
+	}{
+		Status: "ok",
+	}
+
+	// Кодируем нашу структуру в JSON и отправляем ее в качестве ответа.
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	// Регистрируем наш обработчик.
+	// Теперь все запросы на адрес "/api/health" будет обрабатывать функция healthCheckHandler.
+	http.HandleFunc("/api/health", healthCheckHandler)
+
+	// Запускаем сервер на порту 8080.
+	log.Println("Сервер запускается на порту :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Ошибка при запуске сервера: ", err)
+	}
+}
